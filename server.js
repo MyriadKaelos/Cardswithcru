932e4abff07d3db7ba00804b7bb69f018734411e
Yates Lough <Yates@Josephs-MacBook-Pro.local>
2019-03-07 14:48:56 -0800

    Commit Second...

--- server.js
+++ server.js
@@ -12,24 +12,29 @@ app.set('view engine','ejs');
 // get an instance of the express Router
 var router = express.Router();
 
-// a “get” at the root of our web app: http://localhost:3000/api
-// router.get('/', function(req, res) {
-//   db.collection('questions').find({}).toArray((err, questions) => {
-//     db.collection('answers').find({}).toArray((err, answers) => {
-//       if(err) {console.log(err)}
-//       res.render('index.ejs', {questions: questions,answers: answers})
-//     })
-//   })
-// });
-
 router.get('/',function(req, res) {
   res.render('index.ejs');
 })
 
+router.post('/logout', function(req,res) {
+
+})
 router.post('/startGame', function(req,res) {
-  res.render('game.ejs');  //renders index page in browser
+  res.render('login.ejs')
 })
 
+router.post('/addUser', function(req,res) {
+  db.collection('questions').find({}).toArray((err, questions) => {
+    db.collection('answers').find({}).toArray((err, answers) => {
+      var data = req.body.name
+      db.collection('users').insertOne({name:data})
+      db.collection('users').find({}).toArray((err, users) => {
+        if(err) {console.log(err)}
+        res.render('add.ejs', {questions: questions,answers: answers,users: users})
+      })
+    })
+  })
+})
 router.post('/addCards', function(req,res) {
   db.collection('questions').find({}).toArray((err, questions) => {
       db.collection('answers').find({}).toArray((err, answers) => {
@@ -43,14 +48,14 @@ router.post('/newAnswer', function(req, res) {
   console.log("New answer sent.");  //logs to terminal
   var data = req.body.answer
   db.collection('answers').insertOne({answer:data})
-  res.render('index.ejs');  //renders index page in browser
+  res.render('add.ejs');  //renders index page in browser
 });
 
 router.post('/newQuestion', function(req, res) {
   console.log("New question sent.");  //logs to terminal
   var data = req.body.question
   db.collection('question').insertOne({question:data})
-  res.render('index.ejs');  //renders index page in browser
+  res.render('add.ejs');  //renders index page in browser
 });
 
 
@@ -65,7 +70,8 @@ MongoClient.connect('mongodb://yateslough:Yateslough1@ds029638.mlab.com:29638/ca
     console.log("Connected successfully to server");
 
     db = client.db('cardswithcru');
-    app.listen(3000, () => {
-        console.log('Our ears are open to your cries.')
-    })
+    app.listen(process.env.PORT || 3000,function(){
+        console.log("listening on 3000");
+        console.log("Our ears are open to your cries");
+    });
 })
