30c01a7283430e67d084220de530e3723dfef183
Yates Lough <Yates@Josephs-MacBook-Pro.local>
2019-03-14 23:04:03 -0700

    Commit Second...

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-// call the packages we need
-var express    = require('express')      // call express
-var bodyParser = require('body-parser')
-var app = express()     // define our app using express
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert');
-
-// configure app to use bodyParser() and ejs
+const MongoClient = require('mongodb').MongoClient;
+var express       = require('express');
+var bodyParser    = require('body-parser');
+var app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine','ejs');
-
-// get an instance of the express Router
 var router = express.Router();
-
-// a “get” at the root of our web app: http://localhost:3000/api
-// router.get('/', function(req, res) {
-//   db.collection('questions').find({}).toArray((err, questions) => {
-//     db.collection('answers').find({}).toArray((err, answers) => {
-//       if(err) {console.log(err)}
-//       res.render('index.ejs', {questions: questions,answers: answers})
-//     })
-//   })
-// });
-
 router.get('/',function(req, res) {
   res.render('index.ejs');
 })
+app.use('/', router);
 
-router.post('/startGame', function(req,res) {
-  res.render('game.ejs');  //renders index page in browser
+router.post('/logout', function(req,res) {
+
+})
+router.post('startGame',function(req,res) {
+  res.render('game.ejs')
+})
+//Renders the Log In page.
+router.post('/login', function(req,res) {
+  res.render('login.ejs', {user: "false"})
 })
 
+router.post('/addUser', function(req,res) {
+  db.collection('questions').find({}).toArray((err, questions) => {
+    db.collection('answers').find({}).toArray((err, answers) => {
+      if(err) {console.log(err)}
+      var data = req.body.name
+      db.collection('users').insertOne({name:data})
+      res.render('login.ejs', {user:data})
+    })
+  })
+})
+
+//Renders the Add Cards Page.
 router.post('/addCards', function(req,res) {
   db.collection('questions').find({}).toArray((err, questions) => {
       db.collection('answers').find({}).toArray((err, answers) => {
@@ -43,29 +46,37 @@ router.post('/newAnswer', function(req, res) {
   console.log("New answer sent.");  //logs to terminal
   var data = req.body.answer
   db.collection('answers').insertOne({answer:data})
-  res.render('index.ejs');  //renders index page in browser
+  res.render('add.ejs');  //renders index page in browser
 });
 
 router.post('/newQuestion', function(req, res) {
   console.log("New question sent.");  //logs to terminal
   var data = req.body.question
   db.collection('question').insertOne({question:data})
-  res.render('index.ejs');  //renders index page in browser
+  res.render('add.ejs');  //renders index page in browser
 });
 
 
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+router.post('/home', function(req, res) {
+  res.render('index.ejs');
+})
+//XMLHttp
+//=================
+function getUsers() {
+  db.collection('users').find({}).toArray((err, answers) => {
+      if(err) {console.log(err)}
+  })
+}
 
+
+//=================
 // START THE SERVER
 //==========================================================
-var db
-MongoClient.connect('mongodb://yateslough:Yateslough1@ds029638.mlab.com:29638/cardswithcru',{ useNewUrlParser: true }, (err, client) => {
-    if(err) console.log(err)
+MongoClient.connect('mongodb://yateslough:Yateslough1@ds029638.mlab.com:29638/cardswithcru', {useNewUrlParser:true}, (err, client) => {
+    if(err) { console.log(err) }
     console.log("Connected successfully to server");
-
-    db = client.db('cardswithcru');
-    app.listen(3000, () => {
-        console.log('Our ears are open to your cries.')
-    })
-})
+    db = client.db('cardswithcru')
+    app.listen(process.env.PORT || 5000,function(){
+        console.log("listening on 5000");
+    });
+});
