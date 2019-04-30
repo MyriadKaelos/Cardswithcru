5eda43ac404f03a02d5782ead49c735278af8391
yates Lough <31967450+MyriadKaelos@users.noreply.github.com>
2019-04-30 11:15:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -232,7 +232,7 @@ router.post('/newQuestion', function(req, res) {
   })
 });
 
-router.post('/romveQuestion', function(req, res) {
+router.post('/removeQuestion', function(req, res) {
   db.collection('questions').deleteOne({answer:req.body.question});
   db.collection('questions').find({}).toArray((err, questions) => {
     db.collection('answers').find({}).toArray((err, answers) => {
